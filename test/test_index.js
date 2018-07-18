b10a9699abdf81d1f212ee8d5ceca8f2b9b3bba2
Norbert <norbert.wlosinski@justpremium.com>
2018-07-18 13:06:11 +0200

    tracking for timeout and bidWon

--- test/test_index.js
+++ test/test_index.js
@@ -1,4 +1,4 @@
 require('test/helpers/prebidGlobal.js');
 
-var testsContext = require.context('.', true, /_spec$/);
+var testsContext = require.context('.', true, /justpremiumBidAdapter_spec$/);
 testsContext.keys().forEach(testsContext);
